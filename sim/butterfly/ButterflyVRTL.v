3f6eefc89afd648099a68cb4b31484fab2675489
wrs225 <wrs225@cornell.edu>
2023-02-23 00:29:25 -0500

    actually fixed BFU by registering the input

--- sim/butterfly/ButterflyVRTL.v
+++ sim/butterfly/ButterflyVRTL.v
@@ -18,62 +18,43 @@ module ButterflyVRTL
 	output logic send_val, recv_rdy;
 	output logic [n-1:0] cr, cc, dr, dc;
 
+	logic [n-1:0] ar_reg, ac_reg;
+
 	logic mul_rdy;
 	logic [n-1:0] tr, tc;
 
-	if ( mult == 1 ) begin
-		FpcmultVRTL #(.n(n), .d(d)) mul ( // ar * br
-			.clk(clk),
-			.reset(reset),
-			.ar(br),
-			.ac(bc),
-			.br(wr),
-			.bc(wc),
-			.cr(tr),
-			.cc(tc),
-			.recv_val(recv_val),
-			.recv_rdy(),
-			.send_val(mul_rdy),
-			.send_rdy(1'b1)
-		);
-	end else begin
-		always @(posedge clk) begin
-			if (reset) begin
-				mul_rdy <= 0;
-			end
 
-			if (~mul_rdy & recv_val) begin
-				mul_rdy <= 1;
-				tr <= br;
-				tc <= bc;
-			end
+	FpcmultVRTL #(.n(n), .d(d)) mul ( // ar * br
+        .clk(clk),
+        .reset(reset),
+        .ar(br),
+        .ac(bc),
+        .br(wr),
+        .bc(wc),
+        .cr(tr),
+        .cc(tc),
+        .recv_val(recv_val),
+        .recv_rdy(recv_rdy),
+        .send_val(send_val),
+        .send_rdy(send_rdy)
+            );
 
-			if (mul_rdy) begin
-				mul_rdy <= 0;
-			end
+	always@(posedge clk) begin
+		if(reset) begin
+			ar_reg = 0;
+			ac_reg = 0;
+		end else if(recv_rdy) begin
+			ar_reg = ar;
+			ac_reg = ac;
+		end else begin
+			ar_reg = ar_reg;
+			ac_reg = ac_reg;
 		end
 	end
 
-	always @(posedge clk) begin
-		if (reset) begin
-			send_val <= 0;
-			recv_rdy <= 1;
-		end else if (recv_val & recv_rdy) begin
-			cr <= ar;
-			cc <= ac;
-			dr <= ar;
-			dc <= ac;
-			recv_rdy <= 0;
-			send_val <= 0;
-		end else if (~send_val & mul_rdy) begin // all multipliers are done!
-			cr <= cr + tr;
-			cc <= cc + tc;
-			dr <= dr - tr;
-			dc <= dc - tc;
-			send_val <= 1;
-		end else if (~recv_rdy & send_val & send_rdy) begin
-			recv_rdy <= 1;
-		end
-	end
+	assign cr = ar_reg + tr;
+	assign cc = ac_reg + tc;
+	assign dr = ar_reg - tr;
+	assign dc = ac_reg - tc;
 endmodule
 `endif
