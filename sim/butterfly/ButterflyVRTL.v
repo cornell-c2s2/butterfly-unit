536e2c1be7f879f3a3a26d68bdbb64aa3c9d35bf
UnsignedByte <edlam1889@gmail.com>
2023-04-18 16:37:50 -0700

    fix butterfly bug

--- sim/butterfly/ButterflyVRTL.v
+++ sim/butterfly/ButterflyVRTL.v
@@ -1,7 +1,7 @@
 `ifndef PROJECT_BUTTERFLY_V
 `define PROJECT_BUTTERFLY_V
 `include "../../../fixedpt-iterative-complex-multiplier/sim/cmultiplier/FpcmultVRTL.v"
-`include "../../../butterfly-unit/sim/butterfly/RegisterV.v"
+`include "../../../C2S2-Module-Library/lib/sim/nbitregister/RegisterV_Reset.v"
 
 module ButterflyVRTL
 
@@ -31,96 +31,101 @@ module ButterflyVRTL
 
 	logic [n-1:0] tr, tc;
 	generate
-		if (mult == 1) begin
-			always @(posedge clk) begin
-				if (reset) begin
-					cr = 0; cc = 0; dr = 0; dc = 0;
-					send_val = 0;
-				end else if (recv_val & recv_rdy) begin
-					cr = ar + br; cc = ac + bc; dr = ar - br; dc = ac - bc;
-					send_val = 1;
-				end else if (send_val & send_rdy) begin
-					send_val = 0;
-				end else begin
-					cr = cr; cc = cc; dr = dr; dc = dc;
-					send_val = send_val;
+		case (mult)
+			1: begin
+				always @(posedge clk) begin
+					if (reset) begin
+						cr <= 0; cc <= 0; dr <= 0; dc <= 0;
+						send_val <= 0;
+					end else if (recv_val & recv_rdy) begin
+						cr <= ar + br; cc <= ac + bc; dr <= ar - br; dc <= ac - bc;
+						send_val <= 1;
+					end else if (send_val & send_rdy) begin
+						send_val <= 0;
+					end else begin
+						cr <= cr; cc <= cc; dr <= dr; dc <= dc;
+						send_val <= send_val;
+					end
 				end
+				assign recv_rdy = ~send_val;
 			end
-			assign recv_rdy = ~send_val;
-		end else if (mult == 2) begin
-			always @(posedge clk) begin
-				if (reset) begin
-					cr = 0; cc = 0; dr = 0; dc = 0;
-					send_val = 0;
-				end else if (recv_val & recv_rdy) begin
-					cr = ar - br; cc = ac - bc; dr = ar + br; dc = ac + bc;
-					send_val = 1;
-				end else if (send_val & send_rdy) begin
-					send_val = 0;
-				end else begin
-					cr = cr; cc = cc; dr = dr; dc = dc;
-					send_val = send_val;
+			2: begin
+				always @(posedge clk) begin
+					if (reset) begin
+						cr <= 0; cc <= 0; dr <= 0; dc <= 0;
+						send_val <= 0;
+					end else if (recv_val & recv_rdy) begin
+						cr <= ar - br; cc <= ac - bc; dr <= ar + br; dc <= ac + bc;
+						send_val <= 1;
+					end else if (send_val & send_rdy) begin
+						send_val <= 0;
+					end else begin
+						cr <= cr; cc <= cc; dr <= dr; dc <= dc;
+						send_val <= send_val;
+					end
 				end
+				assign recv_rdy = ~send_val;
 			end
-			assign recv_rdy = ~send_val;
-		end else if (mult == 3) begin
-			always @(posedge clk) begin
-				if (reset) begin
-					cr = 0; cc = 0; dr = 0; dc = 0;
-					send_val = 0;
-				end else if (recv_val & recv_rdy) begin
-					cr = ar - bc; cc = ac + br; dr = ar + bc; dc = ac - br;
-					send_val = 1;
-				end else if (send_val & send_rdy) begin
-					send_val = 0;
-				end else begin
-					cr = cr; cc = cc; dr = dr; dc = dc;
-					send_val = send_val;
+			3: begin
+				always @(posedge clk) begin
+					if (reset) begin
+						cr <= 0; cc <= 0; dr <= 0; dc <= 0;
+						send_val <= 0;
+					end else if (recv_val & recv_rdy) begin
+						cr <= ar - bc; cc <= ac + br; dr <= ar + bc; dc <= ac - br;
+						send_val <= 1;
+					end else if (send_val & send_rdy) begin
+						send_val <= 0;
+					end else begin
+						cr <= cr; cc <= cc; dr <= dr; dc <= dc;
+						send_val <= send_val;
+					end
 				end
+				assign recv_rdy = ~send_val;
 			end
-			assign recv_rdy = ~send_val;
-		end else if (mult == 4) begin
-			always @(posedge clk) begin
-				if (reset) begin
-					cr = 0; cc = 0; dr = 0; dc = 0;
-					send_val = 0;
-				end else if (recv_val & recv_rdy) begin
-					cr = ar + bc; cc = ac - br; dr = ar - bc; dc = ac + br;
-					send_val = 1;
-				end else if (send_val & send_rdy) begin
-					send_val = 0;
-				end else begin
-					cr = cr; cc = cc; dr = dr; dc = dc;
-					send_val = send_val;
+			4: begin
+				always @(posedge clk) begin
+					if (reset) begin
+						cr <= 0; cc <= 0; dr <= 0; dc <= 0;
+						send_val <= 0;
+					end else if (recv_val & recv_rdy) begin
+						cr <= ar + bc; cc <= ac - br; dr <= ar - bc; dc <= ac + br;
+						send_val <= 1;
+					end else if (send_val & send_rdy) begin
+						send_val <= 0;
+					end else begin
+						cr <= cr; cc <= cc; dr <= dr; dc <= dc;
+						send_val <= send_val;
+					end
 				end
+				assign recv_rdy = ~send_val;
 			end
-			assign recv_rdy = ~send_val;
-		end else begin
-			FpcmultVRTL #(.n(n), .d(d)) mul ( // ar * br
-				.clk(clk),
-				.reset(reset),
-				.ar(br),
-				.ac(bc),
-				.br(wr),
-				.bc(wc),
-				.cr(tr),
-				.cc(tc),
-				.recv_val(recv_val),
-				.recv_rdy(recv_rdy),
-				.send_val(send_val),
-				.send_rdy(send_rdy)
-			);
-			RegisterV #(.BIT_WIDTH(n)) ac_reg(.clk(clk), .w(recv_rdy), .d(ac), .q(ac_imm), .reset(reset)); //TODO make register resettable
-			RegisterV #(.BIT_WIDTH(n)) ar_reg(.clk(clk), .w(recv_rdy), .d(ar), .q(ar_imm), .reset(reset)); //TODO make register resettable
+			default: begin
+				FpcmultVRTL #(.n(n), .d(d)) mul ( // ar * br
+					.clk(clk),
+					.reset(reset),
+					.ar(br),
+					.ac(bc),
+					.br(wr),
+					.bc(wc),
+					.cr(tr),
+					.cc(tc),
+					.recv_val(recv_val),
+					.recv_rdy(recv_rdy),
+					.send_val(send_val),
+					.send_rdy(send_rdy)
+				);
+				RegisterV_Reset #(.N(n)) ac_reg(.clk(clk), .w(recv_rdy), .d(ac), .q(ac_imm), .reset(reset)); 
+				RegisterV_Reset #(.N(n)) ar_reg(.clk(clk), .w(recv_rdy), .d(ar), .q(ar_imm), .reset(reset));
 
-			
-		end
+				assign cr = ar_imm + tr;
+				assign cc = ac_imm + tc;
+				assign dr = ar_imm - tr;
+				assign dc = ac_imm - tc;				
+			end
+		endcase
 	endgenerate
 	
-	assign cr = ar_imm + tr;
-	assign cc = ac_imm + tc;
-	assign dr = ar_imm - tr;
-	assign dc = ac_imm - tc;				
 
 	
 endmodule
